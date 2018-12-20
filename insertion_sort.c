1ed7a8deb3858886bbd1c3b5af464e034803c10a
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-12-20 12:13:45 -0600

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,1 +1,12 @@
-insertion_sort
+insertion_sort(item s[], int n)
+{
+    int i,j;
+    
+    for (i=1; i<n; i++) {
+        j=i;
+        while ((j>0) && (s[j] < s[j-1])) {
+                swap(&s[j],&s[j-1]);
+                j = j-1;
+        }
+    }
+}
