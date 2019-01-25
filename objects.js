a04477a651d825ba8f4e1bd984a5696120b50779
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2019-01-25 15:27:43 -0600

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,10 @@
+var strPrimitive = "I am a string";
+typeof strPrimitive;							// "string"
+strPrimitive instanceof String;					// false
+
+var strObject = new String( "I am a string" );
+typeof strObject; 								// "object"
+strObject instanceof String;					// true
+
+// inspect the object sub-type
+Object.prototype.toString.call( strObject );	// [object String]
